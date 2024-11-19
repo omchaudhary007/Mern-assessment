1d4da75bdb86e22b84b114db1dde3f552b63a64c
Om chaudhary <167326628+omchaudhary007@users.noreply.github.com>
2024-11-19 18:15:41 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
       setLoading(true);
       const options = {
         credentials: "include",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ userName, password }),
       };
       const result = await post("/admin/login", options);
