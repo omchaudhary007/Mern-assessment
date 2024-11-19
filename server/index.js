17b062608c5d6de1dcbb710596ceec5b07e01ba6
Om chaudhary <167326628+omchaudhary007@users.noreply.github.com>
2024-11-19 18:03:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,13 @@ cloudinary.config({
 
 // middlewares
 
-app.use(cors());
+const corsOptions = {
+  origin: ["https://merntask-taupe.vercel.app", "http://localhost:5173"],
+  credentials: true, 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
